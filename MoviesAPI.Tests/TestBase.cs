caa10d29665ffb2e4d03f2e76aaeb40aa54076fe
Bryan <bryanmonterom@gmail.com>
2023-04-08 16:28:08 -0400

    Create ReviewController Unit Tests

--- MoviesAPI.Tests/TestBase.cs
+++ MoviesAPI.Tests/TestBase.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
 using MoviesAPI.Helpers;
 using NetTopologySuite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,16 +16,23 @@ namespace MoviesAPI.Tests
 {
     public class TestBase
     {
+       protected string rolAdminId = "9aae0b6d-d50c-4d0a-9b90-2a6873e3845d";
+        protected string usuarioAdminId = "5673b8cf-12de-44f6-92ad-fae4a77932ad";
+        string userEmail = "bmontero@hotmail.com";
         //To build a db for each test
-        protected ApplicationDbContext BuildContext(string dbName) {
-            
+        protected ApplicationDbContext BuildContext(string dbName)
+        {
+
+
+
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(dbName).Options;
             var dbContext = new ApplicationDbContext(options);
             return dbContext;
-        
+
         }
 
-        protected IMapper ConfigureAutoMapper() {
+        protected IMapper ConfigureAutoMapper()
+        {
 
             var config = new MapperConfiguration(options =>
             {
@@ -32,6 +43,22 @@ namespace MoviesAPI.Tests
             });
 
             return config.CreateMapper();
+        }
+
+        protected ControllerContext BuildControllerContext()
+        {
+            var usuario = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+            new Claim(ClaimTypes.Name as string, userEmail as string),
+            new Claim(ClaimTypes.Email as string, userEmail as string),
+            new Claim(ClaimTypes.NameIdentifier as string, usuarioAdminId as string),
+
+            }));
+
+            return new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = usuario }
+            };
         }
     }
 }
---
+++ MoviesAPI.Tests/UnitTests/ReviewsControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Controllers;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Tests.UnitTests
+{
+    [TestClass]
+    public class ReviewsControllerTests:TestBase
+    {
+        [TestMethod]
+        public async Task UserCannotCreate2Reviews() { 
+        
+            var dbName = Guid.NewGuid().ToString(); 
+            var context = BuildContext(dbName);
+            CreateMovies(dbName);
+
+            var movieId = await context.Movies.Select(a=> a.Id).FirstOrDefaultAsync();
+            var review1 = new Review() {MovieId = movieId, UserId = usuarioAdminId, Rate = 5 };
+            context.Add(review1);
+            await context.SaveChangesAsync();
+
+            var context2 = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+
+            var controller = new ReviewsController(context2,mapper);
+            controller.ControllerContext = BuildControllerContext();
+
+            var reviewCreationDTO = new ReviewCreationDTO() {  Rate = 5 };
+            var response = await controller.Post(movieId,reviewCreationDTO);
+
+            var value = response as IStatusCodeActionResult;
+            Assert.AreEqual(400, value.StatusCode.Value);
+
+
+        }
+
+        [TestMethod]
+        public async Task CreateReview() {
+            var dbName = Guid.NewGuid().ToString();
+            var context = BuildContext(dbName);
+            CreateMovies(dbName);
+
+            var movieId = await context.Movies.Select(a => a.Id).FirstOrDefaultAsync();
+
+
+            var context2 = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            var controller = new ReviewsController(context2, mapper);
+            controller.ControllerContext = BuildControllerContext();
+            var reviewCreationDTO = new ReviewCreationDTO() { Rate = 5 };
+
+
+            var response = await controller.Post(movieId, reviewCreationDTO);
+
+
+            var value = response as NoContentResult;
+            Assert.IsNotNull(value);
+
+            var context3 = BuildContext(dbName);
+            var reviewDB = context3.Reviews.First();
+
+            Assert.AreEqual(usuarioAdminId, reviewDB.UserId);
+
+
+
+
+
+        }
+
+        private async void CreateMovies(string dbName) {
+
+            var context = BuildContext(dbName);
+            context.Add(new Movie() { Title = "Movie 1" });
+            await context.SaveChangesAsync();
+        }
+    }
+}
